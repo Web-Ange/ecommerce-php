ba7dd6f89e0830a0c767b209403d9f574bc4a993
Bennet Otoho <bennet2000@outlook.com>
2024-03-22 12:56:32 +0100

    more realistic phone numbers

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,12 +31,25 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $phone_number = 0;
+        switch ($this->faker->numberBetween(1, 3)) {
+            case 1:
+                $phone_number = $this->faker->numberBetween(6999999999, 7200000000);
+                break;
+            case 2:
+                $phone_number = $this->faker->numberBetween(7999999999, 8200000000);
+                break;
+            case 3:
+                $phone_number = $this->faker->numberBetween(8999999999, 9200000000);
+                break;
+        }
+
         return [
             'username' => $this->faker->unique()->username(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'email' => $this->faker->unique()->safeEmail(),
             'full_name' => $this->faker->name(),
-            'phone_number' => $this->faker->numberBetween(69999999999, 10000000000),
+            'phone_number' => $phone_number,
         ];
     }
 
