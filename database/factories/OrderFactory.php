1729e5cb7316577bdc49c7e4c54fe06305b6d6fd
Marycollete Nduka <marycollectnduka@gmail.com>
2024-05-03 14:19:37 -1100

    Model Category And Category Factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -13,13 +13,13 @@ class OrderFactory extends Factory
      */
     public function definition()
     {
+
         return [
-            'user_id' => $this->faker->unique()->user id(),
-            'order_date' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'status' => $this->faker->unique()->status(),
-            'full_name' => $this->localizedFaker->name(),
-            'phone_number' => $phone_number,
-            //
+            
+            'user_id' => $this->faker->numberBetween(1, 100),
+            'order_date' => $this->faker->dateTime(),
+            'total_amount' => $this->faker->randomFloat(2, 10, 500),
+            'status' => $this->faker->randomElement(['processing', 'shipped', 'delivered']),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Order;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,8 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(25)->create();
-        Product::factory(200)->create();
+        //User::factory(25)->create();
+       // Product::factory(200)->create();
         // Order::factory(30)->create();
+        Category::factory(15)->create();
     }
 }
